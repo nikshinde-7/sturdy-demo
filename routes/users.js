d33e91c3d99760eb7bf5dabcfef52a5340a6d075
nikshinde-7 <nshinde@deqode.com>
2022-07-04 12:47:18 +0530

    Updated the route fil;e

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({data: [{username: "John", id: 1},{username: "Jake", id: 2}] });
+router.get('/', function (req, res, next) {
+  res.send({ data: [{ username: "John Jackobs", id: 1 }, { username: "Jake Gynehellal", id: 2 }] });
 });
 
 module.exports = router;
