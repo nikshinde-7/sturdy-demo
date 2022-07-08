90155a32be008f0826968c6fac7a97e659fb3b2f
nikshinde-7 <nshinde@deqode.com>
2022-07-08 11:18:03 +0530

    Added few updates to index file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Deployed Express Application' });
+  res.render('index', { title: 'Deployed Express Application with Ftps' });
 });
 
 module.exports = router;
